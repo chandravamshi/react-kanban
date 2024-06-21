503354bd2940c890f9e0ab1dbaa35e14e7b040e0
Chandra Vamshi Dasyam <chandravamshidasyam@Chandras-Air.fritz.box>
2024-06-21 21:29:38 +0200

    added default vaues and basci components

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useMemo, useState } from "react";
+import { Column, Task } from "../types";
+import { defaultTasks } from "../dataConfig/taskDefaultValues";
+import { defaultCols } from "../dataConfig/ColumnDefaultValues";
 
 function kanbanboard() {
-  return (
-    <div>kanbanboard</div>
-  )
+  const [tasks, setTasks] = useState<Task[]>(defaultTasks);
+  const [columns, setColumns] = useState<Column[]>(defaultCols);
+  const columnIds = useMemo(() => columns.map((col) => col.id), [columns]);
+
+  const [activeColumn, setActiveColumn] = useState<Column | null>(null);
+  const [activeTask, setActiveTask] = useState<Task | null>(null);
+  return <div>kanbanboard</div>;
 }
 
-export default kanbanboard
+export default kanbanboard;
---
+++ src/dataConfig/columnDefaultValues.ts
@@ -0,0 +1,16 @@
+import { Column } from "../types";
+
+export const defaultCols: Column[] = [
+  {
+    id: "todo",
+    title: "Todo",
+  },
+  {
+    id: "doing",
+    title: "Work in progress",
+  },
+  {
+    id: "done",
+    title: "Done",
+  },
+];
